567360d23348d0c0052a6a8ed6b40e26a52d5f39
vianavitor-dev <vianavitor232@gmail.com>
2026-01-31 19:03:37 -0300

    feat: add Dijkstra Algorithm
    implemented following the book instructions

---
+++ src/main/java/com/vianavitor/algorithms/search/DijkstraAlgorithm.java
@@ -0,0 +1,65 @@
+package com.vianavitor.algorithms.search;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class DijkstraAlgorithm {
+    private final Set<String> visited = new HashSet<>();
+
+    private String getNodeWithMinCost(HashMap<String, Integer> costs) {
+        int minCost = Integer.MAX_VALUE;
+        String node = null;
+
+        for (Entry<String, Integer> current : costs.entrySet()) {
+            if (current.getValue() < minCost && !visited.contains(current.getKey())) {
+                minCost = current.getValue();
+                node = current.getKey();
+            }
+        }
+
+        return node;
+    }
+
+    public void search(HashMap<String, HashMap<String, Integer>> graph, HashMap<String, Integer> costs, HashMap<String, String> parents) {
+        String node = getNodeWithMinCost(costs);
+
+        while (node != null) {
+            visited.add(node);
+
+            int cost = costs.get(node);
+
+            if (graph.get(node) == null) {
+                continue;
+            }
+
+            Set<Entry<String, Integer>> neighbors = graph.get(node).entrySet();
+
+            for (Entry<String, Integer> neighbor : neighbors) {
+                String n = neighbor.getKey();
+
+                int neighborCost = neighbor.getValue();
+                int costNodeToNeighbor = cost + neighborCost;
+
+                int currentNeighborCost = costs.get(n);
+
+                if (costNodeToNeighbor < currentNeighborCost) {
+                    costs.put(n, costNodeToNeighbor);
+                    parents.put(n, node);
+                }
+            }
+
+            node = getNodeWithMinCost(costs);
+        }
+    }
+
+    public String showTheMinPath(HashMap<String, String> parents, String last) {
+        if (parents.get(last) == null) {
+            return parents.get(last);
+        }
+
+        return showTheMinPath(parents, parents.get(last)) + " => " + parents.get(last);
+    }
+}
--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -3,14 +3,13 @@ import com.vianavitor.algorithms.dataStructure.VertexExample;
 import com.vianavitor.algorithms.search.BinarySearch;
 import com.vianavitor.algorithms.search.BreadthFirstSearch;
 
+import com.vianavitor.algorithms.search.DijkstraAlgorithm;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class SearchTest {
 
@@ -86,5 +85,33 @@ public class SearchTest {
         boolean result = algorithm.search(graph, true);
 
         Assertions.assertEquals(true, result, "there is no programmer");
+    }
+
+    @Test
+    @Tag("Search")
+    @DisplayName("Dijkstra Algorithm")
+    public void DijkstraAlgorithmTest() {
+        HashMap<String, HashMap<String, Integer>> graph = new HashMap<>();
+        HashMap<String, Integer> costs = new HashMap<>();
+        HashMap<String, String> parents = new HashMap<>();
+
+        graph.put("start", new HashMap<>(Map.of("A", 6, "B", 2)));
+        graph.put("A", new HashMap<>(Map.of("end", 1)));
+        graph.put("B", new HashMap<>(Map.of("A", 3, "end", 5)));
+        graph.put("end", null);
+
+        costs.put("A", 6);
+        costs.put("B", 2);
+        costs.put("end", Integer.MAX_VALUE);
+
+        parents.put("A", "start");
+        parents.put("B", "start");
+        parents.put("end", null);
+
+
+        DijkstraAlgorithm dijkstra = new DijkstraAlgorithm();
+        dijkstra.search(graph, costs, parents);
+
+        dijkstra.showTheMinPath(parents, "end");
     }
 }
