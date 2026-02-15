7e28eea105c64e7446975282f4de0104c2d43865
Vitor Hugo Marques Viana <vianavitor232@gmail.com>
2026-02-15 20:39:03 +0000

    feat: add LongestCommonSubsequence
    also implements Dynamic Programming

--- src/main/java/com/vianavitor/algorithms/dp/LongestCommonSubsequence.java
+++ src/main/java/com/vianavitor/algorithms/dp/LongestCommonSubsequence.java
@@ -1,18 +1,13 @@
 package com.vianavitor.algorithms.dp;
 
-import java.lang.Math;
-
-/*
-    This class are going to implement the longest comum substring as described into the book
-*/
-public class LongestComumSubstring {
+public class LongestCommonSubsequence {
     public static int execute(String s1, String s2) {
         int max = 0; // the result
 
         int n1 = s1.length();
         int n2 = s2.length();
 
-        int[][] table = new int[n1][n2]; // the table that is going to register the longest substrings
+        int[][] table = new int[n1][n2]; // the table that is going to register the longest subsequence
 
         for (int x = 0; x < n1; x++) {
             for (int y = 0; y < n2; y++) {
@@ -20,7 +15,6 @@ public class LongestComumSubstring {
                 int yy = 0;
 
                 if (s1.charAt(x) == s2.charAt(y)) {
-
                     if (x-1 >= 0 && y-1 >= 0) {
                         xx = x-1;
                         yy = y-1;
@@ -28,6 +22,11 @@ public class LongestComumSubstring {
 
                     table[x][y] = table[xx][yy] +1;
                     max = Math.max(max, table[x][y]);
+                } else {
+                    if (x -1 >= 0) xx = x-1;
+                    if (y -1 >= 0) yy = y-1;
+
+                    table[x][y] = Math.max(table[x][yy], table[xx][y]);
                 }
 
                 System.out.print("| %d ".formatted(table[x][y]));
--- src/test/java/LongestCommonSubsequenceTest.java
+++ src/test/java/LongestCommonSubsequenceTest.java
@@ -1,23 +1,21 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import com.vianavitor.algorithms.dp.LongestComumSubstring;
+import com.vianavitor.algorithms.dp.LongestCommonSubsequence;
 
-public class LongestComumSubstringTest {
+public class LongestCommonSubsequenceTest {
     
     @ParameterizedTest()
     @CsvSource({
         "blue, clues, 3",
         "hish, fish, 3",
-        "forth, fish, 1",
-        "fosh, fish, 2"
+        "forth, fish, 2",
+        "fosh, fish, 3"
     })
     public void testAlgorithm(String str1, String str2, int expectedResult) {
         System.out.println("NEW TEST: " + str1 + ", " + str2);
-        assertEquals(expectedResult, LongestComumSubstring.execute(str1, str2));
+        assertEquals(expectedResult, LongestCommonSubsequence.execute(str1, str2));
     }
 }
