29362d7072671259b8607025cbb08fe27ba69e32
Vitor Hugo Marques Viana <vianavitor232@gmail.com>
2026-02-15 20:04:57 +0000

    feat: add LongestComumSubstring class
    implemented to record the book content related to Dynamic Programing

---
+++ src/main/java/com/vianavitor/algorithms/dp/LongestComumSubstring.java
@@ -0,0 +1,40 @@
+package com.vianavitor.algorithms.dp;
+
+import java.lang.Math;
+
+/*
+    This class are going to implement the longest comum substring as described into the book
+*/
+public class LongestComumSubstring {
+    public static int execute(String s1, String s2) {
+        int max = 0; // the result
+
+        int n1 = s1.length();
+        int n2 = s2.length();
+
+        int[][] table = new int[n1][n2]; // the table that is going to register the longest substrings
+
+        for (int x = 0; x < n1; x++) {
+            for (int y = 0; y < n2; y++) {
+                int xx = 0; 
+                int yy = 0;
+
+                if (s1.charAt(x) == s2.charAt(y)) {
+
+                    if (x-1 >= 0 && y-1 >= 0) {
+                        xx = x-1;
+                        yy = y-1;
+                    }
+
+                    table[x][y] = table[xx][yy] +1;
+                    max = Math.max(max, table[x][y]);
+                }
+
+                System.out.print("| %d ".formatted(table[x][y]));
+            }
+            System.out.println("|\n");
+        }
+
+        return max;
+    }
+}
---
+++ src/test/java/LongestComumSubstringTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.vianavitor.algorithms.dp.LongestComumSubstring;
+
+public class LongestComumSubstringTest {
+    
+    @ParameterizedTest()
+    @CsvSource({
+        "blue, clues, 3",
+        "hish, fish, 3",
+        "forth, fish, 1",
+        "fosh, fish, 2"
+    })
+    public void testAlgorithm(String str1, String str2, int expectedResult) {
+        System.out.println("NEW TEST: " + str1 + ", " + str2);
+        assertEquals(expectedResult, LongestComumSubstring.execute(str1, str2));
+    }
+}
